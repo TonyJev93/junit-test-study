94fa6e9c532a599fceb209909dae00c8317a039b
JUN GYEWON <tonyjev93@gmail.com>
2022-09-29 23:42:48 +0900

    test: 요구사항 3

--- src/test/java/com/tonyjev/study/junit/string/StringTest.java
+++ src/test/java/com/tonyjev/study/junit/string/StringTest.java
@@ -3,8 +3,11 @@ package com.tonyjev.study.junit.string;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringTest {
 
@@ -57,4 +60,48 @@ class StringTest {
         }
     }
 
+    @Nested
+    @DisplayName("요구사항 3")
+    class Require3 {
+        @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
+        @ParameterizedTest(name = "위치 {1} = {0}")
+        @CsvSource({
+                "a,0",
+                "b,1",
+                "c,2",
+        })
+        void require3_1(char expected, int charPosition) {
+            // given
+            var target = "abc";
+
+            // when
+            var result = target.charAt(charPosition);
+
+            //then
+            assertThat(result).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
+        void require3_2() {
+            // given
+            var target = "abc";
+            var minIndexMinusOne = 0 - 1;
+            var maxIndexPlusOne = target.length() + 1;
+            var expectedExceptionType = StringIndexOutOfBoundsException.class;
+
+            // when & then
+            assertThatThrownBy(() -> {
+                target.charAt(minIndexMinusOne);
+            }).as("최소 Index 보다 1 작은 경우")
+                    .isInstanceOfAny(expectedExceptionType)
+                    .hasMessageContaining(String.format("String index out of range: " + minIndexMinusOne));
+
+            assertThatThrownBy(() -> {
+                target.charAt(maxIndexPlusOne);
+            }).as("최대 Index 보다 1 큰 경우")
+                    .isInstanceOfAny(expectedExceptionType)
+                    .hasMessageContaining("String index out of range: " + maxIndexPlusOne);
+        }
+    }
 }
