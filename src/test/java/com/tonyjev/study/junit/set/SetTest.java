82b7963a1a2267329a10498206343ef52c15e116
JUN GYEWON <tonyjev93@gmail.com>
2022-09-29 23:55:41 +0900

    test: Set 테스트 요구사항 1

--- src/test/java/com/tonyjev/study/junit/set/SetTest.java
+++ src/test/java/com/tonyjev/study/junit/set/SetTest.java
@@ -1,9 +1,14 @@
 package com.tonyjev.study.junit.set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SetTest {
     private Set<Integer> numbers;
@@ -16,4 +21,20 @@ class SetTest {
         numbers.add(2);
         numbers.add(3);
     }
+
+    @Nested
+    @DisplayName("요구사항 1")
+    class Require1 {
+        @Test
+        @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
+        void require1() {
+            // when
+            var size = numbers.size();
+            var expected = 3;
+
+            //then
+            assertThat(size).isEqualTo(expected);
+        }
+    }
+
 }
