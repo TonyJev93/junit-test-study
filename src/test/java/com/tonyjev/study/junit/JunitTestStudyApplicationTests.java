23b0c6c522395a191d22f3724e55e1ac326ea74c
JUN GYEWON <tonyjev93@gmail.com>
2022-09-29 22:40:54 +0900

    init

---
+++ src/test/java/com/tonyjev/study/junit/JunitTestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyjev.study.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTestStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
